5c4be488826998d2faf0057aafb9bb610f1fa10b
debasis-gif <debasis_pal_123@yahoo.co.uk>
2022-03-07 06:57:00 +0000

    DevOps Live Project Commit#16 from Linux - dynamic_inventory.sh

--- Deliverables/vpc_ec2_terraform/dynamic_inventory.sh
+++ Deliverables/vpc_ec2_terraform/dynamic_inventory.sh
@@ -1,6 +1,8 @@
 PUB_IP=`aws ec2 describe-instances --filters Name=tag:Name,Values=tf-instance --region ap-southeast-1 | grep 'PublicIpAddress' | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])'`
 echo $PUB_IP
-cat <<EOF > ../ansible-plays/inventory.ini
+ANS=/var/lib/jenkins/workspace/terraform-ansible-prov-conf/Deliverables/ansible-plays/
+echo 'creating inventory.ini...' > $ANS/inventory.ini
+cat <<EOF > $ANS/inventory.ini
 [all:vars]
 ansible_ssh_user=ubuntu
 ansible_ssh_private_key_file=~/.ssh/spring-petclinic.pem
